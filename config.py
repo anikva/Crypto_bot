4e00e49385e6fa3a1b9cc9386653825447a26959
anikva <103490049+anikva@users.noreply.github.com>
2022-09-15 14:38:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "5487682248:AAFngjJEe_3HuRYfdgYsNRrgsNKSAz9r3Iw"
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import keys
+
+class ConvertionExсeption(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionExсeption(f'Невозможно перевести одинаковые валюты {base}!')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExсeption(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExсeption(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionExсeption(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = float(json.loads(r.content)[keys[base]])
+        return total_base
